d08aa1d03e93291b69ea1001d623238fab0a23bc
chen <gakiclin@gmail.com>
2018-07-25 13:53:55 +0800

    add shorthand method

--- src/service/index.js
+++ src/service/index.js
@@ -13,6 +13,24 @@ class Service extends BaseService {
     this.app = app;
   }
   
+  unwrapList (result) {
+    return (result && result.data) ? result.data : []
+  }
+
+  async findOne (condition) {
+    const query = { where: condition, $limit: 1 }
+    const result = this.unwrapList(await this.find(query))
+    return result && result.length ? result[0] : undefined
+  }
+
+  async findNaked (condition) {
+    return this.unwrapList(await this.find(condition))
+  }
+
+  async findAllNaked () {
+    return this.findNaked({ $limit: this.paginate.max || 100 })
+  }
+  
   static create (options) {
     debug('create service: ', options)
     return new Service(options)
