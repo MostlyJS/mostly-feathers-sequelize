290509629a05f4c871c3a7b44636175268d4dd26
forafish <fora.luo@gmail.com>
2019-06-12 15:42:04 +0800

    Fix default options

--- src/service/index.js
+++ src/service/index.js
@@ -26,7 +26,7 @@ const defaultOptions = {
 
 class Service extends BaseService {
   constructor(options) {
-    options = Object.assign({}, options, defaultOptions)
+    options = Object.assign({}, defaultOptions, options)
     super(options)
   }
 
