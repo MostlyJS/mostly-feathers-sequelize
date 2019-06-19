c1126f9d520e8a8b2ec078ab2bb242bdcacf537f
forafish <fora.luo@gmail.com>
2019-06-19 19:36:34 +0800

    populate hook for find

--- src/hooks/populate.js
+++ src/hooks/populate.js
@@ -1,18 +1,35 @@
+const { isArray, find, map } = require('lodash')
+const sequelize = require('sequelize')
+const Op = sequelize.Op
+
 module.exports = function populate(service, field) {
   return async context => {
 
-    async function simpleJoin (item) {
+    async function populateWith (items) {
       const resource = context.app.service(service)
-      if (resource && item[field]) {
-        const result = await resource.first({ query: { id: item[field] } })
-        if (result) {
-          item[field] = result
-        }
+      const ids = map(items, field)
+      const results = await resource.find({
+        query: { id: { [Op.in]: ids } },
+        paginate: false
+      })
+      if (results) {
+        return map(items, item => {
+          const obj = find(results, { id: item[field] })
+          if (obj) {
+            item[field] = obj
+          }
+          return item
+        })
+      } else {
+        return items
       }
-      return item
     }
 
-    context.result.data = await simpleJoin(context.result.data)
+    if (isArray(context.result.data)) {
+      context.result.data = await populateWith(context.result.data)
+    } else {
+      context.result.data = (await populateWith([context.result.data]))[0]
+    }
     return context
   }
 }
